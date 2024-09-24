ba396701fb6b183df64b755c105014df0003f29b
helcio <h.andre@giap.pl>
2024-09-24 18:47:17 +0100

    Add testimonials images

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,46 @@
 module HomeHelper
+
+  def get_faq
+    faq = [
+      {
+        question: 'Como posso me inscrever como profissional na plataforma?',
+        answer:
+          'Para se inscrever, basta acessar a nossa página de cadastro, preencher as informações necessárias, carregar os seus certificados e/ou diplomas, e submeter o cadastro para avaliação. Após a aprovação, poderá manter os seus serviços disponíveis.',
+      },
+      {
+        question:
+          'Como as empresas podem solicitar serviços na plataforma Freelancers?',
+        answer:
+          'As empresas podem se cadastrar na plataforma e descrever as suas necessidades específicas no campo de solicitação. De seguida, a nossa equipe conecta as empresas aos profissionais mais qualificados para atender ao serviço pretendido.',
+      },
+      {
+        question: 'Quais são os serviços que a plataforma Freelancers oferece? ',
+        answer:
+          'Oferecemos uma ampla gama de serviços profissionais, desde consultoria, acessória, formações, e até desenvolvimento de projetos específicos, sempre conectando as necessidades das empresas com os profissionais disponíveis.',
+      },
+      {
+        question:
+          'Como a plataforma Freelancers garante a qualidade dos profissionais cadastrados? ',
+        answer:
+          'Realizamos um rigoroso processo de seleção e avaliação para garantir que apenas profissionais qualificados e experientes façam parte da nossa plataforma, assegurando a excelência dos serviços prestados.',
+      },
+      {
+        question: 'Qual é o custo para utilizar a plataforma Freelancers? ',
+        answer:
+          'A nossa plataforma não implica algum custo de utilização, porém, a cobrança ou investimento é feito com base na solicitação de serviços ou profissionais pretendido por, ou para quem os solicita. Neste caso enviaremos uma proposta orçamental ao requerente. ',
+      },
+      {
+        question:
+          'Para informações adicionais, como posso entrar em contacto, além da plataforma?',
+        answer:
+          'Poderá contactar-nos ligando para os terminais telefónicos, ou enviando um e-mail o através da aba de contato / fale connosco indicados aqui no site, o nosso serviço de atendimento ao cliente, estará inteiramente disponível para ajudar!',
+      },
+      {
+        question: 'Quais são as regiões em que a plataforma Freelancers actua?',
+        answer:
+          'Atuamos em todo o território nacional, conectando profissionais e empresas de diversas áreas e regiões para promover parcerias de sucesso.',
+      },
+    ]
+  end
+
 end
