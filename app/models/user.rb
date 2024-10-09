65471b693c39316db6677c7fe03fb2763b058f52
helcio <h.andre@giap.pl>
2024-10-09 18:23:36 +0100

    Add advise gem and configure pt for notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, :last_name, presence: true, length: {minimum:3, maximum:3}
+  validates :cellphone, format: { with: /\A(9[123456]\d{7}|\+244\d{9})\z/, message: "Deve ser um número de telemóvel válido" }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
 end
