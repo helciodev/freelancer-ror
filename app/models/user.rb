0a12cc1e1b164f4015bd20e373b3c00eca7b407c
helcio <h.andre@giap.pl>
2024-10-10 20:43:50 +0100

    Style login and signup forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :first_name, :last_name, presence: true, length: {minimum:3, maximum:3}
-  validates :cellphone, format: { with: /\A(9[123456]\d{7}|\+244\d{9})\z/, message: "Deve ser um número de telemóvel válido" }
+  validates :first_name, :last_name, presence: true
+  validates :cellphone, format: { with: /\^(9[123456]\d{7}|\+244\d{9})\z/, message: "Deve ser um número de telemóvel válido" }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
